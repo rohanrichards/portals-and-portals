718ad8272fe520d5c5963b7259b0bd1972d143d6
rohanrichards <rohan.m.richards@gmail.com>
2017-08-09 13:31:45 +1000

    set number of players
    code in place for setting number of players

--- GraphicalDisplayManager.py
+++ GraphicalDisplayManager.py
@@ -12,6 +12,41 @@ class GraphicalDisplayManager:
 
 
     def drawPlayerSetupScreen(self, maxPlayers, playersList, tokens, selectedTokens):
+        self.setNumberOfPlayers(maxPlayers, playersList)
+        self.setNameAndTokens(playersList, tokens, selectedTokens)
+
+    def setNumberOfPlayers(self, maxPlayers, playersList):
+        def setNumPlayers(number):
+            for index, player in enumerate(playersList):
+                if index < number:
+                    print("set player to non-ai")
+                    player.ai = False
+                else:
+                    print("set player to ai")
+                    player.ai = True
+            root.destroy()
+
+        root = Tk()
+        root.title("Setup Game")
+
+        mainFrame = ttk.Frame(root, padding="10 10 10 10")
+        mainFrame.grid(column=0, row=0, sticky=(N, W, E, S))
+        mainFrame.columnconfigure(0, weight=1)
+        mainFrame.rowconfigure(0, weight=1)
+
+        ttk.Label(mainFrame, text="How many players?").grid(column=0, row=0, sticky=(W, E))
+        buttonFrame = ttk.Frame(mainFrame, padding="10 10 10 10")
+        buttonFrame.grid(column=0, row=3)
+
+        # draw the tokens
+        for i in range(1, maxPlayers+1):
+                b = ttk.Button(buttonFrame, text=i,
+                               command=lambda number=i: setNumPlayers(number))
+                b.grid(column=i, row=0)
+
+        root.mainloop()
+
+    def setNameAndTokens(self, playersList, tokens, selectedTokens):
         def setToken(player, name, token):
             print("selected token" + token)
             print(player.name)
@@ -48,8 +83,16 @@ class GraphicalDisplayManager:
                         b.grid(column=index, row=0)
 
                 root.mainloop()
-
-
+            else:
+                player.name = "Bot " + str(index)
+                print('Player {} is now known as {}'.format(index + 1, player.name))
+                for token in tokens:
+                    if token not in selectedTokens:
+                        player.token = token;
+                        selectedTokens.append(token);
+                        print('{} has been allocated {} for their game token '.format(player.name,
+                                                                                      player.token))
+                        break
 
     def drawBoard(self, menuOptions=None, isAi=False):
         pass
